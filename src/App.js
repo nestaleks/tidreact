351b64c4d33748b2f8bbf7bd3f8710b78eb24130
nestaleks <140888222+nestaleks@users.noreply.github.com>
2024-10-29 11:15:42 +0200

    app init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
